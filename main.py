f71706d58b600e7a8f893d3dda9a117e4f5b1854
Miguel Cortes <miguel_cortes1@yahoo.com>
2019-07-18 22:35:12 -0500

    go baby bo

--- main.py
+++ main.py
@@ -9,28 +9,27 @@ db = SQLAlchemy(app)
 app.secret_key = "y33hgn77"
 
 class Blog(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.Text)
-    owner_id = db.Column(db.Integer,  db.ForeignKey('user.id'))
-    
-    
-    def __init__(self, title, body):
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
         
-
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
-    blogz = db.relationship('Blog', backref='owner')
+    blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
-        self.password = password 
+        self.password = password
+     
 
 @app.before_request
 def require_login():
@@ -103,7 +102,10 @@ def logout():
     return redirect('/index')
 
 @app.route('/newpost', methods=['POST', 'GET'])
+
 def new_post():
+    owner = User.query.filter_by(username=session['username']).first()
+
     if request.method == 'POST':
         blog_title = request.form['blog-title']
         blog_body = request.form['blog-entry']
@@ -117,7 +119,7 @@ def new_post():
             new_entry = Blog(blog_title, blog_body)     
             db.session.add(new_entry)
             db.session.commit()        
-            return redirect('/blog?id={}'.format(new_entry.id)) 
+            return redirect('/newpost?id={}'.format(new_entry.id)) 
         else:
             return render_template('newpost.html', title='New Entry', 
                 blog_title=blog_title, blog_body=blog_body)
